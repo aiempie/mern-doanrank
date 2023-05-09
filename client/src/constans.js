70c571243a505ab6caf36eab7612536c688622cf
khanhtn <khanhtnfptdn@gmail.com>
2023-05-09 23:01:34 +0700

    fix env variable

--- client/src/constans.js
+++ client/src/constans.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5005/api/v1"
-    : "deploy url";
+    : process.env.REACT_APP_API_URL;
 
 export const LOCAL_STORAGE_TOKEN_NAME = "accessToken";
 export const LOCAL_STORAGE_CLIP = "fetchClip";
